115d171379eb89ea9791abd95bc6ab949a634bcb
Michael Ko <michko09@hotmail.com>
2017-10-09 15:11:57 -0700

    Passed half the cases when testing them individually.

--- f17-lab4-michko09/src/test/java/threeDpaths/ThreeDWalksTest.java
+++ f17-lab4-michko09/src/test/java/threeDpaths/ThreeDWalksTest.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class ThreeDWalksTest extends ThreeDWalks {
-	
+	/*
 	@Test
 	public void test000() {
 		assertEquals(1, ThreeDWalks.numPaths(0, 0, 0));
@@ -20,22 +20,22 @@ public class ThreeDWalksTest extends ThreeDWalks {
 	public void test112() {
 		assertEquals(2, ThreeDWalks.numPaths(1, 1, 2));
 	}
-	/*
+	
 	@Test
 	public void test050() {
 		assertEquals(1, ThreeDWalks.numPaths(0, 5, 0));
 	}
-
+	
 	@Test
 	public void test123() {
 		assertEquals(1, ThreeDWalks.numPaths(1, 2, 3));
 	}
-
+	*/
 	@Test
 	public void test222() {
 		assertEquals(0, ThreeDWalks.numPaths(2, 2, 2));
 	}
-
+	/*
 	@Test
 	public void test333() {
 		assertEquals(12, ThreeDWalks.numPaths(3, 3, 3));
